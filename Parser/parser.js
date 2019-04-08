a6ab01274f7d5bf69bf576c936e9531d8faee83f
KryptoCrash <kryptocrashmh@gmail.com>
2019-04-08 16:09:16 -0400

    Create parser.js

---
+++ Parser/parser.js
@@ -0,0 +1,35 @@
+class Parser {
+    constructor(stream) {
+        this.stream = stream
+        this.bp = {
+            '=': 1,
+            '+': 2,
+            '-': 2,
+            '*': 3,
+            '/': 3,
+            '%': 3
+        }
+    }
+    expr(rbp = 0) {
+        let left = this.stream.next()
+        if (left.type == 'groupStart') {
+            let str = ''
+            while (this.stream.peek().type != 'groupEnd') {
+                str += this.stream.next().value
+            }
+            stream.next()
+            return this.led(new Parser(new Lexer(new charStream(str))).expr(), stream.next())
+        }
+        while (this.bp[this.stream.peek().value] > rbp) {
+            left = this.led(left, this.stream.next())
+        }
+        return left
+    }
+    led(left, operator) {
+        return {
+            left: left,
+            operator: operator,
+            right: this.expr(this.bp[operator.value])
+        }
+    }
+}
