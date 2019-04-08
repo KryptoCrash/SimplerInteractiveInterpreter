aea55700c90065f352dc3cd5ffdbc223f9d62682
KryptoCrash <kryptocrashmh@gmail.com>
2019-04-08 16:10:31 -0400

    Update charStream.js

--- Lexer/charStream.js
+++ Lexer/charStream.js
@@ -1,15 +1,15 @@
 class charStream {
-  constructor(program) {
-    this.index = 0
-    this.program = program.split('').filter(c => !((/\s/).test(c)))
-  }
-  next() {
-    return this.program[this.index++]
-  }
-  peek() {
-    return this.program[this.index]
-  }
-  continue() {
-    return !!(this.peek())
-  }
+    constructor(program) {
+        this.index = 0
+        this.program = program.split('').filter(c => !((/\s/).test(c)))
+    }
+    next() {
+        return this.program[this.index++]
+    }
+    peek() {
+        return this.program[this.index]
+    }
+    continue () {
+        return !!(this.peek())
+    }
 }
